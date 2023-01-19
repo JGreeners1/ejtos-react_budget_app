2d2063c3b655ae5ad36f7fd1e626c1e0d32d5bad
JGreeners1 <jack.tg@hotmail.co.uk>
2023-01-19 10:21:55 -0500

    update

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -52,7 +52,7 @@ const AllocationForm = (props) => {
                   </select>
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
-                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation </label>
                 </div>
                   <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
